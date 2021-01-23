3d1cc4ed344968ec3815b690fc595093aebc68b1
GRIETIOTLAB <77530550+GRIETIOTLAB@users.noreply.github.com>
2021-01-23 13:17:06 +0530

    Add files via upload

---
+++ ECWS_Relay.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run o,OUTPUT);nce:
+pinMode(19,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(19,HIGH);
+delay(1000);
+digitalWrite(19,LOW);
+delay(1000);
+}
