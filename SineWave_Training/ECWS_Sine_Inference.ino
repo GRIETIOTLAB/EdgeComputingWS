420370f18bf48450a9f2cd7ab6be69af4233d8b2
GRIETIOTLAB <77530550+GRIETIOTLAB@users.noreply.github.com>
2021-01-28 12:10:16 +0530

    Add files via upload

---
+++ SineWave_Training/ECWS_Sine_Inference.ino
@@ -0,0 +1,29 @@
+#include <EloquentTinyML.h>
+#include "model.h"
+
+#define NUMBER_OF_INPUTS 1
+#define NUMBER_OF_OUTPUTS 1
+
+#define TENSOR_ARENA_SIZE 2*1024
+
+Eloquent::TinyML::TfLite<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> ml;
+void setup() {
+    Serial.begin(115200);
+    ml.begin(model_data);
+}
+
+void loop() {
+    // pick up a random x and predict its sine
+    float x = 3.14 * random(100) / 100;
+    float y = sin(x);
+    float input[1] = { x };
+    float predicted = ml.predict(input);
+
+    Serial.print("sin(");
+    Serial.print(x);
+    Serial.print(") = ");
+    Serial.print(y);
+    Serial.print("\t predicted: ");
+    Serial.println(predicted);
+    delay(1000);
+}
